98bcf49c3f88caf59f8be421a20c18a3e3dfa9b8
chen yuan <chenyuan@iwown.com>
2016-04-19 20:00:10 +0800

    自动登录

--- fabdemo/ViewController.m
+++ fabdemo/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import "SUCache.h"
 
 @interface ViewController ()
 
@@ -26,16 +27,51 @@
                                              selector:@selector(_updateContent:)
                                                  name:FBSDKProfileDidChangeNotification
                                                object:nil];
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(_accessTokenChanged:)
                                                  name:FBSDKAccessTokenDidChangeNotification
                                                object:nil];
     
+    SUCacheItem *item = [SUCache itemForSlot:0];
+     [self labelDisplayWithProfile:item.profile];
+    
+}
+
+#pragma mark - login
+//自定义login button的点击事件
+- (IBAction)loginBtnClicked:(id)sender {
+    NSInteger slot = 0;
+    FBSDKAccessToken *token = [SUCache itemForSlot:slot].token;
+    if (token) {
+        [self autoLoginWithToken:token];
+    }
+    else {
+        [self newLogin];
+    }
 
 }
 
-//自定义login button的点击事件
-- (IBAction)loginBtnClicked:(id)sender {
+- (void)autoLoginWithToken:(FBSDKAccessToken *)token {
+    [FBSDKAccessToken setCurrentAccessToken:token];
+    FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:nil];
+    [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        //token过期，删除存储的token和profile
+        if (error) {
+            NSLog(@"The user token is no longer valid.");
+            NSInteger slot = 0;
+            [SUCache deleteItemInSlot:slot];
+            [FBSDKAccessToken setCurrentAccessToken:nil];
+            [FBSDKProfile setCurrentProfile:nil];
+        }
+        //做登录完成的操作
+        else {
+            
+        }
+    }];
+}
+
+- (void)newLogin {
     FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
     [login
      logInWithReadPermissions: @[@"public_profile"]
@@ -54,6 +90,31 @@
 
 
 
+#pragma mark - Notification
+
+- (void)_updateContent:(NSNotification *)notification {
+    FBSDKProfile *profile = notification.userInfo[FBSDKProfileChangeNewKey];
+    [self labelDisplayWithProfile:profile];
+}
+
+- (void)_accessTokenChanged:(NSNotification *)notification
+{
+    FBSDKAccessToken *token = notification.userInfo[FBSDKAccessTokenChangeNewKey];
+    if (!token) {
+        [FBSDKAccessToken setCurrentAccessToken:nil];
+        [FBSDKProfile setCurrentProfile:nil];
+    } else {
+        NSInteger slot = 0;
+        SUCacheItem *item = [SUCache itemForSlot:slot] ?: [[SUCacheItem alloc] init];
+        if (![item.token isEqualToAccessToken:token]) {
+            item.token = token;
+            [SUCache saveItem:item slot:slot];
+        }
+    }
+}
+
+#pragma mark - other
+
 //这是facebook sdk中自带的button样式
 - (void)addFacebookButton {
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
@@ -61,13 +122,14 @@
     [self.view addSubview:loginButton];
 }
 
-- (void)_updateContent:(NSNotification *)notification {
-    self.infoLabel.text = [NSString stringWithFormat:@"name = %@,userID = %@",[FBSDKProfile currentProfile].name,[FBSDKProfile currentProfile].userID];
-}
-
-- (void)_accessTokenChanged:(NSNotification *)notification
-{
-    NSLog(@"accessToken changed");
+- (void)labelDisplayWithProfile:(FBSDKProfile *)profile{
+    NSInteger slot = 0;
+    if (profile) {
+        SUCacheItem *cacheItem = [SUCache itemForSlot:slot];
+        cacheItem.profile = profile;
+        [SUCache saveItem:cacheItem slot:slot];
+        self.infoLabel.text = [NSString stringWithFormat:@"name = %@,userID = %@",cacheItem.profile.name,cacheItem.profile.userID];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
